c7e83b932cf7d9552f049369ac9423308142283e
Derek Etman <dereketman@Gmail.com>
2022-09-08 14:46:11 -0700

    Fixed Music & Video pages

--- src/pages/Music.js
+++ src/pages/Music.js
@@ -3,30 +3,25 @@ import React from "react";
 import DefaultLayout from "../layouts/DefaultLayout";
 
 const Music = () => {
+  const musicContainer = {
+    width: "100%",
+    height: "90vh",
+  };
   return (
     <DefaultLayout>
-      <Grid container>
-        <Grid item xs={12}>
-          {/* <iframe
-            width="100%"
-            height="300"
-            // scrolling="no"
-            frameBorder={0}
-            allow="autoplay"
-            src={
-              "https://w.soundcloud.com/player/?url=https%3A//api.soundcloud.com/playlists/564437382"
-            }
-          ></iframe> */}
-
+      <Grid container id={musicContainer}>
+        <Grid item xs></Grid>
+        <Grid item xs={10}>
           <iframe
             width="100%"
             height="300"
             scrolling="no"
-            frameborder="no"
+            frameBorder={0}
             allow="autoplay"
             src="https://w.soundcloud.com/player/?url=https%3A//api.soundcloud.com/playlists/564437382&color=%23ff5500&auto_play=false&hide_related=false&show_comments=true&show_user=true&show_reposts=false&show_teaser=true&visual=true"
           ></iframe>
         </Grid>
+        <Grid item xs></Grid>
       </Grid>
     </DefaultLayout>
   );
--- src/pages/Videos.js
+++ src/pages/Videos.js
@@ -1,20 +1,30 @@
+import { Grid } from "@mui/material";
 import React from "react";
 import DefaultLayout from "../layouts/DefaultLayout";
 
 const Videos = () => {
+  const videoContainer = {
+    width: "100%",
+    height: "90vh",
+  };
   return (
     <DefaultLayout>
-      <div style={{height:"100%"}}>
-        <iframe
-          width="100%"
-          height="100%"
-          src="https://www.youtube.com/embed/9Ogee1YHTqU"
-          title="YouTube video player"
-          frameborder="0"
-          allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture"
-          allowfullscreen
-        ></iframe>
-      </div>
+      <Grid container style={videoContainer}>
+        <Grid item xs></Grid>
+        <Grid item xs={9}>
+          <iframe
+            width="100%"
+            height="80%"
+            src="https://www.youtube.com/embed/9Ogee1YHTqU"
+            title="YouTube video player"
+            frameborder="0"
+            allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture"
+            allowfullscreen
+          ></iframe>
+        </Grid>
+        <Grid item xs></Grid>
+
+      </Grid>
     </DefaultLayout>
   );
 };
